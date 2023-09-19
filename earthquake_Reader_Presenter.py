cbc1089d8324f8beab908c254038b2ac61d05a30
Prajjaval Singh <prajjavalsingh@gmail.com>
2023-09-20 04:56:40 +0530

    created class structure

--- earthquake_Reader_Presenter.py
+++ earthquake_Reader_Presenter.py
@@ -8,66 +8,82 @@ import geopandas as gpd
 import matplotlib.pyplot as plt
 from shapely.geometry import Point
 
-#starttime and endtime format yyyy#mm##dd
-URL = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=csv&starttime=2023-09-09&endtime=2023-09-13"
-try:
-    earthquake_data_resp = requests.get( URL )
-    earthquake_data_resp.raise_for_status()
+#starttime and endtime format yyyy-mm-dd
+class DataFetcherAndPresenter:
+    def __init__(self, starttime, endtime ):
+        self.starttime = starttime
+        self.endtime = endtime
 
-    earthquake_csv = StringIO( earthquake_data_resp.text )
-    df = pd.read_csv( earthquake_csv )
+    def fetchdata(self):
+        URL = "https://earthquake.usgs.gov/fdsnws/event/1/query?format=csv&starttime="+self.starttime+"&endtime="+self.endtime
+        try:
+            earthquake_data_resp = requests.get( URL )
+            earthquake_data_resp.raise_for_status()
 
-except requests.exceptions.RequestException as e:
-    print( "Error:", e )
-except StopIteration:
-    print( "The CSV file appears to be empty." )
-except csv.Error as e:
-    print( "CSV Error:", e )
-except Exception as e:
-    print( "An unexpected error occurred:", e )
+            earthquake_csv = StringIO( earthquake_data_resp.text )
+            df = pd.read_csv( earthquake_csv )
 
-conn = sqlite3.connect('EarthQuake.db')
-df.to_sql( 'EarthQuake_Data_Table', conn, if_exists='replace', index=False )
+        except requests.exceptions.RequestException as e:
+            print( "Error:", e )
+        except StopIteration:
+            print( "The CSV file appears to be empty." )
+        except csv.Error as e:
+            print( "CSV Error:", e )
+        except Exception as e:
+            print( "An unexpected error occurred:", e )
 
-conn.commit()
+        self.conn = conn = sqlite3.connect('EarthQuake.db')
+        df.to_sql( 'EarthQuake_Data_Table', conn, if_exists='replace', index=False )
 
-cursor = conn.cursor()
-query = 'SELECT * FROM EarthQuake_Data_Table ORDER BY time ASC'
-try:
-    cursor.execute( query )
-except Exception as e:
-    print( "Cursor execution raised exception : ", e )
+        conn.commit()
 
-rows = cursor.fetchall()
+# cursor = conn.cursor()
+# query = 'SELECT * FROM EarthQuake_Data_Table ORDER BY time ASC'
+# try:
+#     cursor.execute( query )
+# except Exception as e:
+#     print( "Cursor execution raised exception : ", e )
 
-print(rows[0])
+# rows = cursor.fetchall()
 
-query = 'SELECT latitude FROM EarthQuake_Data_Table'
-cursor.execute( query )
-lat = cursor.fetchone()
-latitudes = []
-while lat is not None:
-    latitudes.append(lat[0])
-    lat = cursor.fetchone()
+# print(rows[0])
+    def getLatLongCoordinates(self):
+        query = 'SELECT latitude FROM EarthQuake_Data_Table'
+        cursor = self.conn.cursor()
+        cursor.execute( query )
+        lat = cursor.fetchone()
+        latitudes = []
+        while lat is not None:
+            latitudes.append(lat[0])
+            lat = cursor.fetchone()
 
-query = 'SELECT longitude FROM EarthQuake_Data_Table'
-cursor.execute( query )
-lon = cursor.fetchone()
-longitudes = []
-while lon is not None:
-    longitudes.append(lon[0])
-    lon = cursor.fetchone()
+        query = 'SELECT longitude FROM EarthQuake_Data_Table'
+        cursor.execute( query )
+        lon = cursor.fetchone()
+        longitudes = []
+        while lon is not None:
+            longitudes.append(lon[0])
+            lon = cursor.fetchone()
 
-conn.close()
+        coordinates = list( zip(longitudes,latitudes) )
+        return coordinates
 
-coordinates = list( zip(longitudes,latitudes) )
-geometry = [Point(lon,lat) for lon,lat in coordinates]
+    def closeConn(self):
+        self.conn.close()
 
-# Create a GeoDataFrame from the points
-world = gpd.read_file( gpd.datasets.get_path('naturalearth_lowres') )
-ax = world.plot( figsize=(10,10), color='white', edgecolor='black' )
-gdf = gpd.GeoDataFrame( geometry, columns=['geometry'] , crs="EPSG:4326" )
-gdf.plot( ax=ax, marker='^', color='red', markersize=5 )
+    def displayOnWorldMap(self,coordinates):
+        geometry = [Point(lon,lat) for lon,lat in coordinates]
 
-# Show the plot
-plt.show()
+        # Create a GeoDataFrame from the points
+        world = gpd.read_file( gpd.datasets.get_path('naturalearth_lowres') )
+        ax = world.plot( figsize=(10,10), color='white', edgecolor='black' )
+        gdf = gpd.GeoDataFrame( geometry, columns=['geometry'] , crs="EPSG:4326" )
+        gdf.plot( ax=ax, marker='^', color='red', markersize=5 )
+
+        # Show the plot
+        plt.show()
+
+earthquakedata = DataFetcherAndPresenter("2023-09-09","2023-09-13")
+earthquakedata.fetchdata()
+coordinates = earthquakedata.getLatLongCoordinates()
+earthquakedata.displayOnWorldMap( coordinates )
