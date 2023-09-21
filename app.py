350c5e47435b9aadd6010f5d0753ad0e7c9276c4
Prajjaval Singh <prajjavalsingh@gmail.com>
2023-09-22 04:46:19 +0530

    script running but arguments not catched properly

--- app.py
+++ app.py
@@ -11,13 +11,17 @@ def index():
 def earthquake_data_gatherer():
     arg1 = request.form['start_date']
     arg2 = request.form['end_date']
-    args = [ arg1, arg2 ]
+    command = [ 'python', 'earthquake_Reader_Presenter.py', arg1, arg2 ]
+    print(type(arg1))
+    print(type(arg2))
     try:
-        result = subprocess.check_output( 'python', 'earthquake_Reader_Presenter.py', args )
-        return result
+        result = subprocess.run( command, stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            text=True )
     except subprocess.CalledProcessError as e:
         print( "Subprocess error : ", e )
-        return
+
+    return
 
 if  __name__ == '__main__':
     app.run()
