7b04198ca44d06580a273ce24272bffd75fc3042
Prajjaval Singh <prajjavalsingh@gmail.com>
2023-10-01 01:25:53 +0530

    remvoed debug checks

--- app.py
+++ app.py
@@ -14,11 +14,13 @@ def earthquake_data_gatherer():
     enddate = request.form['end_date']
     minmag = request.form['min_mag']
     maxmag = request.form['max_mag']
+
     earthquakedata = erp.DataFetcherAndPresenter(startdate,enddate,minmag,maxmag)
     earthquakedata.fetchdata()
     coordinates, colidxs = earthquakedata.getLatLongCoordinatesAndValidMags()
     fig_html = earthquakedata.displayOnWorldMap( coordinates, colidxs )
+    
     return render_template( 'index.html', plot_html=fig_html )
 
 if  __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
